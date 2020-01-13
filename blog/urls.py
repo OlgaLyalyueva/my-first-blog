33d3365d4a713b3d94c02029c1efd6204a169a67
OlgaLyalyueva <olgalyalyueva@gmail.com>
2020-01-13 15:34:35 +0200

    Added the hiphop page

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('breakdance/', views.breakdance, name='breakdance'),
     path('contemporary/', views.contemporary, name='contemporary'),
+    path('hiphop/', views.hiphop, name='hiphop'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
