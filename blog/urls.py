cb39b8ee0f486ba30cc9d026782b26e3b37baf3f
OlgaLyalyueva <olgalyalyueva@gmail.com>
2020-01-22 12:30:35 +0200

    Add the url for contacts page and change position for the post_list url

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('post/list/', views.post_list, name='post_list'),
+    path('contacts/', views.contacts, name='contacts'),
     path('breakdance/', views.breakdance, name='breakdance'),
     path('contemporary/', views.contemporary, name='contemporary'),
     path('hiphop/', views.hiphop, name='hiphop'),
@@ -14,5 +16,4 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/list/', views.post_list, name='post_list'),
 ]
