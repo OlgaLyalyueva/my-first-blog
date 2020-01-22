88ff3d6a7d16d5ff456b9583385f1e37144e4bc0
OlgaLyalyueva <olgalyalyueva@gmail.com>
2020-01-22 12:32:29 +0200

    Add settings for sending emails

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "olgalyalyueva@gmail.com"
+EMAIL_HOST_PASSWORD = '7d7a4339'
+EMAIL_PORT = '587'
