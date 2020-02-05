c5495c881ece957d9d1f974e2ce07edd8db58aa3
OlgaLyalyueva <olgalyalyueva@gmail.com>
2020-02-05 10:28:05 +0200

    Add url for registration pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('loggedout/', views.loggedout, name='loggedout'),
 
 # urls for main tabs
