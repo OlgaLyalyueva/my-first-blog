7cb3c73bef26ad15a67b8a02ea60836663263d93
OlgaLyalyueva <olgalyalyueva@gmail.com>
2020-02-12 15:53:36 +0200

    Add image field to the new_post page

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
+
+
 def loggedout(request):
     return render(request, 'blog/loggedout.html')
 
@@ -93,7 +95,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
